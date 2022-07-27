28d5a13b0bcdf7cd88409684134d47f0e605b906
alex62320 <alex.62320@live.fr>
2022-07-27 10:52:45 +0200

    ajout de cours
    ajout de bootstrap et sass

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,9 +6,11 @@ use App\Entity\Tag;
 use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Writer;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
@@ -17,19 +19,40 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label_attr' => [
+                    'class' => 'test-de-classe'
+                ],
+                'attr' => [
+                    'class' => 'test-classe-input'
+                ],
+                'row_attr' => [
+                    'class' => 'test-classe-div'
+                ]
+            ])
             ->add('body')
             ->add('published_at')
             ->add('tags', EntityType::class, [
                 // looks for choices from this entity
                 'class' => Tag::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.name', 'ASC');
+                },
             
                 // uses the User.username property as the visible option string
                 'choice_label' => 'name',
-            
+                
                 // used to render a select box, check boxes or radios
                 'multiple' => true,
                 'expanded' => true,
+
+                'by_reference' => false,
+
+                'attr' => [
+                    // ajout d'une classe a la div des tag
+                    'class' => 'checkboxes-with-scroll',
+                ]
             ])
             ->add('category', EntityType::class, [
                 // looks for choices from this entity
@@ -37,7 +60,10 @@ class ArticleType extends AbstractType
             
                 // uses the User.username property as the visible option string
                 'choice_label' => 'name',
-            
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
                 // used to render a select box, check boxes or radios
                 // 'multiple' => true,
                 // 'expanded' => true,
@@ -45,7 +71,11 @@ class ArticleType extends AbstractType
             ->add('writer', EntityType::class, [
                 // looks for choices from this entity
                 'class' => Writer::class,
-            
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('w')
+                        ->join('w.user', 'u')
+                        ->orderBy('u.email', 'ASC');
+                },
                 // uses the User.username property as the visible option string
                 'choice_label' => function (Writer $object){
                     return "{$object->getUser()->getEmail()} ({$object->getUser()->getEmail()})";
